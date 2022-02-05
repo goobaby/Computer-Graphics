4b5b8fa7ea2bdd23a1d3013a5493df65378b4574
Amit Deb <deb@sonoma.edu>
2022-02-05 00:04:56 -0800

    Fixed .clear

--- Assignment 2-Basic Rendering/main.js
+++ Assignment 2-Basic Rendering/main.js
@@ -6,14 +6,14 @@ function init()
 {
     var canvas = document.getElementById("webgl-canvas");
     gl = canvas.getContext("webgl2");
-    gl.clearColor(0.0, 0.5, 0.0, 0.0);
-    cone = new Cone(gl, 20);
+    gl.clearColor(0.0, 0.5, 0.0, 1.0);
+    cone = new Cone(gl, 1000);
     render();
 }
 
 function render()
 {
-    gl.clear(gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT);
     cone.render();
 }
 
