086e31da43331fa74d57fc16face67827f56f58c
Amit Deb <deb@sonoma.edu>
2022-02-18 01:01:24 -0800

    Update Cone.js

--- Assignment 3 - Modeling, Animation, and Depth Buffering/Cone.js
+++ Assignment 3 - Modeling, Animation, and Depth Buffering/Cone.js
@@ -1,17 +1,17 @@
 const DefaultNumSides = 8;
 
-function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
+function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
 
     // Initialize the shader pipeline for this object using either shader ids
     //   declared in the application's HTML header, or use the default names.
     //
-    var vertShdr = vertexShaderId || "Cone-vertex-shader";
-    var fragShdr = fragmentShaderId || "Cone-fragment-shader";
+    var vertShdr = vertexShaderId || "Cube-vertex-shader";
+    var fragShdr = fragmentShaderId || "Cube-fragment-shader";
 
     this.program = initShaders(gl, vertShdr, fragShdr);
 
     if ( this.program < 0 ) {
-        alert( "Error: Cone shader pipeline failed to compile.\n\n" +
+        alert( "Error: Cube shader pipeline failed to compile.\n\n" +
             "\tvertex shader id:  \t" + vertShdr + "\n" +
             "\tfragment shader id:\t" + fragShdr + "\n" );
         return; 
@@ -22,14 +22,14 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     var theta = 0.0;
     var dTheta = 2.0 * Math.PI / n;
     
-    // Record the number of components for each vertex's position in the Cone object's 
+    // Record the number of components for each vertex's position in the Cube object's 
     //   positions property. (that's why there's a "this" preceding the positions here).
     //   Here, we both create the positions object, as well as initialize its
     //   numComponents field.
     //
     this.positions = { numComponents : 3 };
     
-    // Initialize temporary arrays for the Cone's indices and vertex positions
+    // Initialize temporary arrays for the Cube's indices and vertex positions
     //
     var positions = [ 0.0, 0.0, 0.0 ];
     var indices = [ 0 ];
@@ -48,18 +48,18 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     indices.push(n);
 
     // Record the number of indices in one of our two disks that we're using to make the
-    //   cone.  At this point, the indices array contains the correct number of indices for a
-    //   single disk, and as we render the cone as two disks of the same size, this value is
+    //   Cube.  At this point, the indices array contains the correct number of indices for a
+    //   single disk, and as we render the Cube as two disks of the same size, this value is
     //   precisely what we need.
     //
     this.indices = { count : indices.length };
 
-    // Now build up the list for the cone.  First, add the apex vertex onto the index list
+    // Now build up the list for the Cube.  First, add the apex vertex onto the index list
     //
     indices.push(n + 1);
 
     // Next, we need to append the rest of the vertices for the permieter of the disk.
-    // However, the cone's perimeter vertices need to be reversed since it's effectively a
+    // However, the Cube's perimeter vertices need to be reversed since it's effectively a
     // reflection of the bottom disk.
     //
     indices = indices.concat( indices.slice(1,n+2).reverse() );
@@ -84,11 +84,11 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
  
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 
-        // Draw the cone's base
+        // Draw the Cube's base
         //
         gl.drawElements( gl.POINTS, this.indices.count, gl.UNSIGNED_SHORT, 0 );
 
-        // Draw the cone's top
+        // Draw the Cube's top
         //
         var offset = this.indices.count * 2 /* sizeof(UNSIGNED_INT) */;
         gl.drawElements( gl.POINTS, this.indices.count, gl.UNSIGNED_SHORT, offset );
