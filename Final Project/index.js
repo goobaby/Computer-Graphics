8bcdd0f713c42117157beed02998d87dc4caa171
Amit Deb <deb@sonoma.edu>
2022-04-29 12:14:44 -0700

    Quick Change projct to garfield earth consumption

--- Final Project/index.js
+++ Final Project/index.js
@@ -8,51 +8,61 @@ const scene = new THREE.Scene()
 
 
 const loader = new GLTFLoader()
-loader.load('assests/burger.glb', function(glb){
-    console.log(glb)
-    const root = glb.scene;
-    root.scale.set(0.1, 0.1, 0.1)
 
-    scene.add(root);
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-}, function(error){
-    console.log('An error occured')
-})
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000
+    })
+)
 
-loader.load('assests/garfield/scene.gltf', function(gltf){
-    console.log(gltf)
-    const garf = gltf.scene;
-    garf.scale.set(0.01, 0.01, 0.01)
-    garf.rotation.y = 10
-    garf.position.set(0,0,-1)
-    scene.add(garf);
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-}, function(error){
-    console.log('An error occured')
-})
+scene.add(sphere)
 
-loader.load('assests/eldgarf/scene.gltf', function(gltf){
-    console.log(gltf)
-    const eldgarf = gltf.scene;
-    eldgarf.scale.set(0.1, 0.1, 0.1)
-    eldgarf.rotation.y = 360
-    eldgarf.position.set(-1,.5,.1)
-    scene.add(eldgarf);
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-}, function(error){
-    console.log('An error occured')
-})
+// loader.load('assests/burger.glb', function(glb){
+//     console.log(glb)
+//     const root = glb.scene;
+//     root.scale.set(0.1, 0.1, 0.1)
+
+//     scene.add(root);
+// }, function(xhr){
+//     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+// }, function(error){
+//     console.log('An error occured')
+// })
+
+// loader.load('assests/garfield/scene.gltf', function(gltf){
+//     console.log(gltf)
+//     const garf = gltf.scene;
+//     garf.scale.set(0.01, 0.01, 0.01)
+//     garf.rotation.y = 10
+//     garf.position.set(0,0,-1)
+//     scene.add(garf);
+// }, function(xhr){
+//     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+// }, function(error){
+//     console.log('An error occured')
+// })
+
+// loader.load('assests/eldgarf/scene.gltf', function(gltf){
+//     console.log(gltf)
+//     const eldgarf = gltf.scene;
+//     eldgarf.scale.set(0.1, 0.1, 0.1)
+//     eldgarf.rotation.y = 360
+//     eldgarf.position.set(-1,.5,.1)
+//     scene.add(eldgarf);
+// }, function(xhr){
+//     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+// }, function(error){
+//     console.log('An error occured')
+// })
 
 
 
 
 
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(2,2,5)
-scene.add(light)
+// const light = new THREE.DirectionalLight(0xffffff, 1)
+// light.position.set(2,2,5)
+// scene.add(light)
 
 
 // const geometry = new THREE.BoxGeometry(1,1,1)
@@ -68,7 +78,7 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
+camera.position.set(0,0,10)
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableZoom = false;
