916b0760c1f0d334f497beb0b9120497329368db
Amit Deb <deb@sonoma.edu>
2022-04-29 12:33:28 -0700

    Earf

--- Final Project/index.js
+++ Final Project/index.js
@@ -12,7 +12,9 @@ const loader = new GLTFLoader()
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     new THREE.MeshBasicMaterial({
-        color: 0xff0000
+        map: new THREE.TextureLoader().load('./assests/globe/basic.jpg')
+        //map: new THREE.TextureLoader().load('./assests/lasagna/textures/Lasagna_baseColor.png')
+        
     })
 )
 
