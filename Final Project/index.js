aff619c08989a96df73aae3399c54889728d37b8
Amit Deb <deb@sonoma.edu>
2022-05-13 20:21:42 -0700

    dear God

--- Final Project/index.js
+++ Final Project/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
-
+import {FBXLoader} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/FBXLoader.js'
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector('.webgl')
@@ -8,7 +8,7 @@ const scene = new THREE.Scene()
 
 
 const loader = new GLTFLoader()
-
+const fLoader = new FBXLoader();
 
 
 
@@ -48,26 +48,58 @@ loader.load('assests/garfield/scene.gltf', function(gltf){
     console.log('An error occured')
 })
 
-// loader.load('assests/eldgarf/scene.gltf', function(gltf){
-//     console.log(gltf)
-//     const eldgarf = gltf.scene;
-//     eldgarf.scale.set(0.1, 0.1, 0.1)
-//     eldgarf.rotation.y = 360
-//     eldgarf.position.set(-1,.5,.1)
-//     scene.add(eldgarf);
-// }, function(xhr){
-//     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
-// }, function(error){
-//     console.log('An error occured')
-// })
+
+loader.load('assests/eldgarf/scene.gltf', function(gltf){
+    console.log(gltf)
+    const eldgarf = gltf.scene;
+    eldgarf.scale.set(1, 1, 1)
+    eldgarf.rotation.y = 360
+    eldgarf.position.set(-1,10,.1)
+    scene.add(eldgarf);
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log('An error occured')
+})
 
 
+// fLoader.load('', function(fbx) {
+//     fbx.scale.setScalar(0.1);
+//     fbx.traverse(c => {
+//         c.castShadow = true;
+//     });
+
+//     const anim = new FBXLoader();
+//     anim.load('assests/aniGaf/animation/Scary Clown Idle.fbx', function(anim) {
+//         this._mixer = new THREE.AnimationMixer(fbx);
+//         const idle = this._mixer.clipAction(anim.animations[0]);
+//         idle.play();
+//     });
+//     scene.add(fbx)
+// });
+
+//mixers=[];
+// fLoader.load('assests/aniGaf/source/garfield_rig_LuksaHD....fbx', (fbx) => {
+//     fbx.scale.setScalar(1);
+//     fbx.traverse(c => {
+//     c.castShadow = true;
+//     });
+
+//     const anim = new FBXLoader();
+//     //anim.setPath('./resources/zombie/');
+//     anim.load('assests/aniGaf/animation/Scary Clown Idle.fbx', (anim) => {
+//     const m = new THREE.AnimationMixer(fbx);
+//     mixers.push(m);
+//     const idle = m.clipAction(anim.animations[0]);
+//     idle.play();
+//     });
+//     this._scene.add(fbx);
+// });
 
 
-
-// const light = new THREE.DirectionalLight(0xffffff, 1)
-// light.position.set(2,2,5)
-// scene.add(light)
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(2,2,5)
+scene.add(light)
 
 
 // const geometry = new THREE.BoxGeometry(1,1,1)
